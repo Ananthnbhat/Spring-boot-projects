b6c209bfdb1db85ab82744ba752fbf8cf4604ba9
anant <ananthnbhat2@gmail.com>
2024-01-12 16:45:30 +1100

    file upload to a local dir & show it using thymeleaf

--- uploading-files/src/main/java/com/project/uploadingfiles/UploadingFilesApplication.java
+++ uploading-files/src/main/java/com/project/uploadingfiles/UploadingFilesApplication.java
@@ -1,13 +1,27 @@
 package com.project.uploadingfiles;
 
+import com.project.uploadingfiles.storage.StorageProperties;
+import com.project.uploadingfiles.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class UploadingFilesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UploadingFilesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UploadingFilesApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
 
 }
